902f3f69c2d41821f34d2548d669fab4f03a08ae
ThisIsKareemG <iamkhova@gmail.com>
2025-03-19 13:00:59 -0400

    Add README and export CryptoChart components
    This commit introduces a new README file with project updates and stages. It also adds exports for CryptoChart-related components and updates the project version to 0.0.1-alpha.203 in package.json. These changes improve documentation and enhance module accessibility.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
+// CryptoChart
+export { default as CryptoChart} from "./components/CryptoChart/CryptoChart"
+export * from "./components/CryptoChart/CryptoChartModel";
+export * from "./components/CryptoChart/LiveDataEntryModel";
+export * from "./components/CryptoChart/TokenDataTypes";
+
+// TickerGUI
 export { default as TickerGUI } from "./components/TickerGUI/components/Ticker";
 export * from "./components/TokenFormatter/model/TokenFormatterModel"
 
+// TickerFormatter
 export { default as TokenFormatter} from "./components/TokenFormatter/component/TokenFormatter";
 export * from "./components/TickerGUI/models/tickerModel"
 
+// UTILS
 export * from "./utils/numberConverter"
-
 
